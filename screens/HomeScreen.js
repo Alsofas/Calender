11af96e2f0e4eefe6bf5aeab3419bfd922c8a909
CPotg <CPotg@sde.local>
2018-05-22 19:30:52 +0200

    Marked dates

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,12 +17,36 @@ export default class HomeScreen extends React.Component {
     header: null,
   }
 
+  state = {
+    clickedDate: '',
+    messages: [
+      { date: '2018-05-20', message:  'Have a nice day!'},
+      { date: '2018-05-21', message:  'Have a nice day 2!'}
+    ]
+  }
+
+  _showMessages = (dateString) => {
+    this.setState({clickedDate: dateString})
+
+
+
+  }
+
   render() {
+    const clickedDate = this.state.clickedDate;
+    const dateToMark = {clickedDate: {selected: true, selectedColor: 'blue'}};
+
+    let messages;
+
+    for(let i = 0; i<this.state.messages; i++) {
+      messages.push(<Text>{this.state.messages[i].message}</Text>)
+    }
+
     return (
       <View style={styles.container}>
         <Calendar
           // Handler which gets executed on day press. Default = undefined
-          onDayPress={(day) => {console.log('selected day', day)}}
+          onDayPress={(day) => { this._showMessages(day.dateString) }}
           // Handler which gets executed on day long press. Default = undefined
           onDayLongPress={(day) => {console.log('selected day', day)}}
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
@@ -41,7 +65,13 @@ export default class HomeScreen extends React.Component {
           onPressArrowLeft={substractMonth => substractMonth()}
           // Handler which gets executed when press arrow icon left. It receive a callback can go next month
           onPressArrowRight={addMonth => addMonth()}
+          // Collection of dates that have to be marked. Default = {}
+         markedDates={{[this.state.clickedDate]: {selected: true, selectedColor: 'blue'}}}
+          // Collection of dates that have to be marked. Default = {}
         />
+        <View>
+          <Text>{this.state.messages[0].message}</Text>
+        </View>
       </View>
     );
   }
